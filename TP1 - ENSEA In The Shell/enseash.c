238d90d7a4e2331e5046a5dbc85a429d71ac6d60
fetohiaras <theo.farias@gmail.com>
2024-11-28 11:06:45 +0100

    Renamed main.c to enseasc.c for consistency

--- TP1 - ENSEA In The Shell/enseash.c
+++ TP1 - ENSEA In The Shell/enseash.c
@@ -7,15 +7,19 @@
 
 #define BUFFER_SIZE 1024
 
-int main (int argc, char *argv[]){
+int main (int argc, char *argv[]) {
     const char *welcome_msg = "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\n";
     write(STDOUT_FILENO, welcome_msg, strlen(welcome_msg));
 
-// REPL loop
+    // Variable to store the status of the last executed command
+    char prompt_status[BUFFER_SIZE] = "";
+
+    // REPL loop
     while (1) {
-        // Print the shell prompt
-        const char *prompt = "enseash % ";
-        write(STDOUT_FILENO, prompt, strlen(prompt));
+        // Print the shell prompt with the last status
+        char prompt[BUFFER_SIZE];
+        int prompt_length = snprintf(prompt, BUFFER_SIZE, "enseash %s%% ", prompt_status);
+        write(STDOUT_FILENO, prompt, prompt_length);
 
         // Adds buffer and reads input
         char command[BUFFER_SIZE];
@@ -28,7 +32,7 @@ int main (int argc, char *argv[]){
             break;
         }
 
-        // Null-terminating the input string and removing trailing newline
+        // Null-terminate the input string and remove trailing newline
         command[bytes_read] = '\0';
         command[strcspn(command, "\n")] = '\0';
 
@@ -61,6 +65,20 @@ int main (int argc, char *argv[]){
             // Parent process: wait for the child to finish
             int status;
             waitpid(pid, &status, 0);
+
+            // Determine the status of the command
+            if (WIFEXITED(status)) {
+                // If the command exited normally, get the exit code
+                int exit_code = WEXITSTATUS(status);
+                snprintf(prompt_status, BUFFER_SIZE, "[exit:%d] ", exit_code);
+            } else if (WIFSIGNALED(status)) {
+                // If the command was terminated by a signal, get the signal number
+                int signal_number = WTERMSIG(status);
+                snprintf(prompt_status, BUFFER_SIZE, "[sign:%d] ", signal_number);
+            } else {
+                // Unknown status
+                strncpy(prompt_status, "[unknown] ", BUFFER_SIZE);
+            }
         }
     }
 
