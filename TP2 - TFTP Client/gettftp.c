b3aa37651018b97c4299ad8610aa377c387a7b52
fetohiaras <theo.farias@gmail.com>
2024-12-09 15:35:32 +0100

    Question 4 added

--- TP2 - TFTP Client/gettftp.c
+++ TP2 - TFTP Client/gettftp.c
@@ -6,37 +6,93 @@
 #include <netdb.h>
 #include <unistd.h>
 
-// Function to resolve the server address and reserve a connection socket
-int create_socket(const char *server) {
-    struct addrinfo hints, *res;
-    int sock;
+#define BUFFER_SIZE 516  // Maximum: 512 bytes of data + 4 bytes of header
 
-    // Configure hints for getaddrinfo
+// Function to build the RRQ packet
+void build_rrq(char *buffer, const char *filename, const char *mode) {
+    uint16_t opcode = htons(1);  // RRQ has opcode 1
+    size_t len = 0;
+
+    // Add opcode
+    memcpy(buffer + len, &opcode, sizeof(opcode));
+    len += sizeof(opcode);
+
+    // Add the filename
+    strcpy(buffer + len, filename);
+    len += strlen(filename) + 1;
+
+    // Add the transfer mode
+    strcpy(buffer + len, mode);
+    len += strlen(mode) + 1;
+
+    write(STDOUT_FILENO, "RRQ packet built\n", 17);
+}
+
+// Function to send the RRQ and receive the first response
+void send_rrq(const char *server, const char *filename) {
+    int sock;
+    struct addrinfo hints, *res;
+    char buffer[BUFFER_SIZE];
+    ssize_t received;
+
+    // Resolve the server address
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;       // IPv4
     hints.ai_socktype = SOCK_DGRAM; // UDP protocol
     hints.ai_protocol = IPPROTO_UDP;
 
-    // Get address information
     if (getaddrinfo(server, "69", &hints, &res) != 0) {
         write(STDOUT_FILENO, "Error resolving server address\n", 31);
         exit(EXIT_FAILURE);
     }
 
     // Create the UDP socket
-    sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (sock == -1) {
+    if ((sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
         write(STDOUT_FILENO, "Error creating socket\n", 23);
         freeaddrinfo(res);
         exit(EXIT_FAILURE);
     }
 
-    write(STDOUT_FILENO, "Socket created successfully\n", 28);
+    // Build the RRQ packet
+    build_rrq(buffer, filename, "octet");
 
-    // Free the addrinfo structure
+    // Send the RRQ packet
+    if (sendto(sock, buffer, strlen(filename) + strlen("octet") + 4, 0, res->ai_addr, res->ai_addrlen) == -1) {
+        write(STDOUT_FILENO, "Error sending RRQ\n", 19);
+        close(sock);
+        freeaddrinfo(res);
+        exit(EXIT_FAILURE);
+    }
+
+    write(STDOUT_FILENO, "RRQ sent to server\n", 20);
+
+    // Prepare to receive the first DATA or ERROR packet
+    struct sockaddr_in sender_addr;
+    socklen_t sender_len = sizeof(sender_addr);
+    received = recvfrom(sock, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&sender_addr, &sender_len);
+    if (received == -1) {
+        write(STDOUT_FILENO, "Error receiving server response\n", 33);
+    } else {
+        uint16_t opcode, block;
+        memcpy(&opcode, buffer, sizeof(uint16_t));
+        opcode = ntohs(opcode);
+
+        if (opcode == 3) {  // DATA packet
+            memcpy(&block, buffer + 2, sizeof(uint16_t));
+            block = ntohs(block);
+            char msg[128];
+            snprintf(msg, sizeof(msg), "Received DATA packet, block %d, size %ld bytes\n", block, received - 4);
+            write(STDOUT_FILENO, msg, strlen(msg));
+        } else if (opcode == 5) {  // ERROR packet
+            write(STDOUT_FILENO, "Error received from server: ", 28);
+            write(STDOUT_FILENO, buffer + 4, strlen(buffer + 4));
+            write(STDOUT_FILENO, "\n", 1);
+        }
+    }
+
+    // Clean up
+    close(sock);
     freeaddrinfo(res);
-
-    return sock;
 }
 
 int main(int argc, char *argv[]) {
@@ -48,17 +104,8 @@ int main(int argc, char *argv[]) {
     const char *server = argv[1];
     const char *file = argv[2];
 
-    write(STDOUT_FILENO, "Resolving server and reserving connection...\n", 45);
-    int sock = create_socket(server);
-
-    write(STDOUT_FILENO, "Server resolved successfully\n", 29);
-    write(STDOUT_FILENO, "File to be downloaded: ", 23);
-    write(STDOUT_FILENO, file, strlen(file));
-    write(STDOUT_FILENO, "\n", 1);
-
-    // Close the socket after use
-    close(sock);
-    write(STDOUT_FILENO, "Socket closed\n", 14);
+    write(STDOUT_FILENO, "Sending RRQ...\n", 16);
+    send_rrq(server, file);
 
     return 0;
 }
